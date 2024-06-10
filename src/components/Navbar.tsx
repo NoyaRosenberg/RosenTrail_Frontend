86fceb8b1ecb9893c55e2e187b81041e10116087
shkediya <shkediya@gmail.com>
2024-06-11 00:28:48 +0300

    Fix Main Page

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Box, Typography, ButtonGroup, Button } from "@mui/material"
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <Box className="nav-bar">
+        <Typography variant="h5">
+          WonderPlan
+        </Typography>
+        <ButtonGroup variant="text" aria-label="Basic button group">
+          <Button component={Link} to="/community">
+            Community Trips
+          </Button>
+          <Button component={Link} to="/signin">
+            Login
+          </Button>
+          <Button component={Link} to="/register">
+            Sign Up
+          </Button>
+        </ButtonGroup>
+      </Box>
+    )
+}
+
+export default Navbar
---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { green } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+        main: green[500], 
+      },
+  },
+});
+
+export default theme;
