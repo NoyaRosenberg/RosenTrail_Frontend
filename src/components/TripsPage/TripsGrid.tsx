f2fb8c9bb0e8198a33810c7db3aefc0cd9396a22
shkediya <shkediya@gmail.com>
2024-06-16 09:47:08 +0300

    Align new trip card to the center when theres no trips

--- src/components/TripsPage/TripsGrid.tsx
+++ src/components/TripsPage/TripsGrid.tsx
@@ -11,15 +11,20 @@ const TripsGrid = ({ trips }: TripsGrisProps) => {
   const navigate = useNavigate();
 
   const createNewTripClick = () => {
-    navigate('/createTrip');
-  }
-  
+    navigate("/createTrip");
+  };
+
   const showTripDetails = () => {
     // navigate to trip details page
-  }
+  };
 
   return (
-    <Grid container spacing={2}>
+    <Grid
+      container
+      spacing={2}
+      display="flex"
+      justifyContent={trips.length == 0 ? "center" : ""}
+    >
       <Grid item xs={12} sm={6} md={4} sx={{ display: "flex" }}>
         <PlaceCard
           name="New Trip"
@@ -29,6 +34,7 @@ const TripsGrid = ({ trips }: TripsGrisProps) => {
           onCardClick={createNewTripClick}
         />
       </Grid>
+
       {trips.map((trip, index) => (
         <Grid item xs={12} sm={6} md={4} key={index} sx={{ display: "flex" }}>
           <PlaceCard
