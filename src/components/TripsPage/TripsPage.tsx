fc3bc588c5df1c924931f33d428a096aa632fc4a
shkediya <shkediya@gmail.com>
2024-07-04 17:33:47 +0300

    Add skeleton in trips page when loading

--- src/components/TripsPage/TripsPage.tsx
+++ src/components/TripsPage/TripsPage.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Box, CircularProgress, Container, Stack, Typography } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
 import SearchBar from "../SearchBar";
 import TripsGrid from "./TripsGrid";
 import { useTrips } from "../../contexts/TripProvider";
 import { Trip } from "../../services/trip.service";
+import CardsSkeleton from "../Skeletons/CardsSkeleton";
 
 const TripsPage = () => {
   const { trips, loading, error, fetchTrips } = useTrips();
@@ -14,33 +15,14 @@ const TripsPage = () => {
   }, [trips]);
 
   const onTripSearch = (searchValue: string) => {
-    const newFilteredTrips = trips.filter(trip =>
-      trip.destinations.some(destination =>
+    const newFilteredTrips = trips.filter((trip) =>
+      trip.destinations.some((destination) =>
         destination.toLowerCase().includes(searchValue.toLowerCase())
       )
     );
 
     setFilteredTrips(newFilteredTrips);
   };
-
-  if (loading) {
-    return (
-      <Container
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          alignContent: "center"
-        }}
-      >
-        <CircularProgress />
-      </Container>
-    );
-  }
-
-  if (error) {
-    return <div>{error}</div>;
-  }
 
   return (
     <Container
@@ -52,7 +34,7 @@ const TripsPage = () => {
         gap: 6,
         paddingTop: "50px",
         paddingBottom: "20px",
-        width: "68%"
+        width: "68%",
       }}
     >
       <Stack spacing={2} alignItems="center" width="100%">
@@ -60,14 +42,21 @@ const TripsPage = () => {
           My Wonderful Trips
         </Typography>
         <Typography variant="overline" display="block" gutterBottom>
-          Create a new trip or enter your former trip to edit, publish or print pictures from your wonderful trips
+          Create a new trip or enter your former trip to edit, publish or print
+          pictures from your wonderful trips
         </Typography>
         <Box width="100%">
           <SearchBar placeholder="Search a trip..." onSearch={onTripSearch} />
         </Box>
       </Stack>
       <Box width="100%">
-        <TripsGrid trips={filteredTrips} fetchTrips={fetchTrips} />
+        {loading ? (
+          <CardsSkeleton numInRow={3} />
+        ) : error ? (
+          <Typography color="error">{error}</Typography>
+        ) : (
+          <TripsGrid trips={filteredTrips} fetchTrips={fetchTrips} />
+        )}
       </Box>
     </Container>
   );
