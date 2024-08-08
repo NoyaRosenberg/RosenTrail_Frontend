1f9d1bc982cf336ec232bceb640ae3731a5afc55
Matan Azarzar <Mat528285@gmail.com>
2024-08-08 23:27:03 +0300

    budget page

--- src/components/BudgetPage/BudgetManagement.tsx
+++ src/components/BudgetPage/BudgetManagement.tsx
@@ -15,10 +15,13 @@ const colors = ["#FF6584", "#00BCD4", "#FF9800", "#4CAF50"];
 
 const BudgetManagement: React.FC<BudgetManagementProps> = ({ budgetData }) => {
   const totalAmount = budgetData.reduce((acc, item) => acc + item.amount, 0);
-  const percentageData = budgetData.map((item) => ({
-    ...item,
-    percentage: ((item.amount / totalAmount) * 100).toFixed(2),
-  }));
+  const percentageData = budgetData
+    .map((item) => ({
+      ...item,
+      percentage: ((item.amount / totalAmount) * 100).toFixed(2),
+    }))
+    .sort((a, b) => b.amount - a.amount)
+    .slice(0, 4);
 
   return (
     <div className="budget-management">
