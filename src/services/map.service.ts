77517c67aaf3bdd7a353f281665cf6b0ce283654
shkediya <shkediya@gmail.com>
2024-08-17 14:41:59 +0300

    Change map marker

--- src/services/map.service.ts
+++ src/services/map.service.ts
@@ -4,47 +4,53 @@ import "leaflet.locatecontrol/dist/L.Control.Locate.min.css";
 import "leaflet.locatecontrol";
 
 class MapService {
-  private map!: L.Map;
+    private map!: L.Map;
 
-  initMap(layers: L.Layer[], center: L.LatLngExpression) {
-    this.map = L.map("map", {
-      center: center,
-      zoom: 14,
-      scrollWheelZoom: true,
-      layers: layers,
-      zoomControl: false,
-    });
-  }
+    initMap(layers: L.Layer[], center: L.LatLngExpression) {
+        this.map = L.map("map", {
+            center: center,
+            zoom: 14,
+            scrollWheelZoom: true,
+            layers: layers,
+            zoomControl: false,
+        });
+    }
 
     removeMap() {
-    this.map.remove();
-  }
+        this.map.remove();
+    }
 
-  addBuildInControls() {
-    L.control
-      .zoom({
-        position: "topright",
-      })
-      .addTo(this.map);
+    addBuildInControls() {
+        L.control
+            .zoom({
+                position: "topright",
+            })
+            .addTo(this.map);
 
-    L.control
-      .locate({
-        position: "bottomright",
-      })
-      .addTo(this.map);
-  }
+        L.control
+            .locate({
+                position: "bottomright",
+            })
+            .addTo(this.map);
+    }
 
-  addControl(control: L.Control) {
-    this.map.addControl(control);
-  }
+    setView(center: L.LatLngExpression) {
+        this.map.setView(center, 14);
+    }
 
-  setView(center: L.LatLngExpression) {
-    this.map.setView(center, 14);
-  }
+    addMarker(point: L.LatLngExpression, name: string) {
+        const customIcon = L.icon({
+            iconUrl: '../../public/marker-icon-red.png',
+            iconSize: [42, 42],
+            iconAnchor: [19, 47],
+            popupAnchor: [2, -45],
+        });
 
-  addMarker(point: L.LatLngExpression, name: string) {
-    L.marker(point).addTo(this.map).bindPopup(name).openPopup();
-  }
+        L.marker(point, {icon: customIcon})
+            .addTo(this.map)
+            .bindPopup(name)
+            .openPopup();
+    }
 }
 
 export default new MapService();
