4dd4c749ba784bf00dfed0426631b3a416f655ea
Noya Rosenberg <noyaro66@gmail.com>
2024-08-17 20:44:22 +0300

    adding refresh token to user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import AuthService from "./auth.service";
 
 export interface User {
   _id: string;
@@ -9,23 +10,39 @@ export interface User {
   age: number;
   phoneNumber: number;
 }
+
 const API_BASE_URL = "http://localhost:3000/users/";
 
-// Create an Axios instance
-const apiClient = axios.create({
-  baseURL: API_BASE_URL,
-});
+const apiClient = axios.create({ baseURL: API_BASE_URL });
 
-// Add a request interceptor to include the token in the headers
-apiClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("authToken");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+apiClient.interceptors.request.use(async (config) => {
+  const token = localStorage.getItem("accessToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => Promise.reject(error));
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    console.log("Interceptor error:", error);
+    if (error.response && error.response.status === 403 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = localStorage.getItem("refreshToken");
+        console.log("Attempting to refresh token with:", refreshToken);
+        const response = await AuthService.refreshToken();
+        console.log("Received new token:", response);
+        localStorage.setItem("accessToken", response as string);
+        apiClient.defaults.headers.common["Authorization"] = `Bearer ${response}`;
+        return apiClient(originalRequest);
+      } catch (refreshError) {
+        console.error("Refresh token error:", refreshError);
+        // Handle refresh token error (e.g., log out user)
+      }
     }
-    return config;
-  },
-  (error) => {
     return Promise.reject(error);
   }
 );
