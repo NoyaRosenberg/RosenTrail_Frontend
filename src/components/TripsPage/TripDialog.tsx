4a9123da22990ab2755b94973d14d3be3a2b7090
Noya Rosenberg <noyaro66@gmail.com>
2024-08-31 21:44:51 +0300

    add budget button

--- src/components/TripsPage/TripDialog.tsx
+++ src/components/TripsPage/TripDialog.tsx
@@ -76,6 +76,10 @@ const TripDialog: React.FC<TripDialogProps> = ({
     navigate("/schedule", { state: { trip, showActions } });
   };
 
+  const showBudget = () => {
+    navigate("/budget", { state: { trip } });
+  };
+
   const handleAddActivity = () => {
     navigate("/AddActivities", { state: { trip } });
   };
@@ -195,14 +199,22 @@ const TripDialog: React.FC<TripDialogProps> = ({
                 <Stack>
                   <Stack>
                     <Stack spacing={5}>
-                      <Box display="flex" alignItems="center" gap="40px">
+                      <Box display="flex" alignItems="center" gap="25px">
                         <Button
                           variant="contained"
                           color="primary"
                           onClick={showSchedule}
-                          sx={{ width: "150px", height: "50px" }}
+                          sx={{ width: "140px", height: "40px" }}
                         >
                           Schedule
+                        </Button>
+                        <Button
+                          variant="contained"
+                          color="error"
+                          onClick={showBudget}
+                          sx={{ width: "140px", height: "40px" }}
+                        >
+                          Budget Page
                         </Button>
                         {!showActions && (
                           <Button
