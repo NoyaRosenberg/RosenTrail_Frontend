6778e987a5220b884cd0560913f29851a581444b
Nadav Samoelov <nadav893@gmail.com>
2024-09-02 01:21:43 +0300

    added overflow to budget

--- src/components/BudgetPage/BudgetPerCategory.tsx
+++ src/components/BudgetPage/BudgetPerCategory.tsx
@@ -13,7 +13,7 @@ const BudgetPerCategory: React.FC<BudgetPerCategoryProps> = ({
   categories,
 }) => {
   return (
-    <div className="budget-per-category">
+    <div className="budget-per-category" style={{ maxHeight: '400px', overflowY: 'auto' }}>
       <h2>Budget Per Category</h2>
       <div className="budget-sliders">
         {categories.map((item, index) => (
